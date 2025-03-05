f17a8b3be1c3b813d49590043b73af61569d0bca
legendsking007 <86476444+legendsking007@users.noreply.github.com>
2025-03-05 08:20:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 export default {
-  name: "Martha", // actual name of the recipient (Mandatory)
+  name: "Kshitiz", // actual name of the recipient (Mandatory)
 
-  nickname: "", // nickname(optional)
+  nickname: "Ronny", // nickname(optional)
 
-  pic: "", // image url of recipients (Mandatory)
+  pic: "https://photos.app.goo.gl/L4cn9jGKVt3opxi27", // image url of recipients (Mandatory)
 
   showScrollMsg: true, // set to false if you do not want the scrolling message
 
-  openDate: "2023-3-28", // the page only can be opened at this time, read README.md
+  openDate: "2025-3-5", // the page only can be opened at this time, read README.md
 
-  timeZone: "Asia/Jakarta", // timezone to use openDate feature
+  timeZone: "Asia/india", // timezone to use openDate feature
 };
